1d61e114b749cd1b1a08ee1af36bc97dafdb91f0
Lelaabk <lolabkari@gmail.com>
2023-03-16 13:51:22 -0700

    Puts a Paragraph

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+/*Write a C program that prints exactly "Programming is like building a multilingual puzzle*/
+int main(void){
+	puts("\"Programming is like building a multilingual puzzle");
+	return(0);
+}
