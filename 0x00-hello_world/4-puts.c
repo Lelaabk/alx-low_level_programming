6758ae89e6ffae0f6972c26955c26fb746089e3e
Lelaabk <lolabkari@gmail.com>
2023-03-16 14:02:03 -0700

    Puts a paragraph

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
-/*Write a C program that prints exactly "Programming is like building a multilingual puzzle*/
-int main(void){
+/**
+ * main - entry point
+ * return: always zero (success)
+ */
+int main(void)
+{
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
