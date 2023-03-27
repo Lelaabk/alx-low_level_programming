c5152168423a1399c08bbf68c546f50e197d92d9
Lelaabk <lolabkari@gmail.com>
2023-03-27 05:08:11 -0700

    Print Array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_array - prints n element of an array of integers
+ * Owned by Bwave
+ * @a: int to check
+ * @n: int to check
+ * Return: 0 is success
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+		if (i != n - 1)
+		printf("%d, ", a[i]);
+		else
+			printf("%d", a[i]);
+printf("\n");
+}
