bd416c13426cd43249d1b9ce7c3a4b0ba0a2134a
Lelaabk <lolabkari@gmail.com>
2023-04-05 05:38:23 -0700

    Length of string

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strlen_recursion - length of string
+ * @s:string
+ * Return:int
+ */
+int _strlen_recursion(char *s)
+{
+	int i = 0;
+
+	if (*s)
+	{
+		i++;
+		i += int _strlen_recursion(s + 1);
+	}
+	return (i);
+}
