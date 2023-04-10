a2ab85c328a43bf51b0e03070a2c6df65e35b398
Lelaabk <lolabkari@gmail.com>
2023-04-10 05:43:57 -0700

    Task 0

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string
+ * @str: the string
+ *
+ * Return: the length of the string
+ */
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str++);
+	}
+	_putchar('\n');
+}
