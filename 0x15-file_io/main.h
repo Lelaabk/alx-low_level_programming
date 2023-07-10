57591610e3fd974626c92c9dd96c086575f1be2b
lelaabk <lolabkari@gmail.com>
2023-07-10 06:41:29 -0700

    Main

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,12 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include systypes.h
-#include sysstat.h
-#include fctnl.h
-#include unistd.h
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
 
-ssize_t read_textfile(const char filename, size_t letters);
+ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char filename, char text_content);
 int append_text_to_file(const char filename, char text_content);
 
