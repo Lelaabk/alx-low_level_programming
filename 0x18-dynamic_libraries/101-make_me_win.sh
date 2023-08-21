4ebf33bd3d0f6eb5f792dac00cf6183aa26dc0ab
Lelaabk <lolabkari@gmail.com>
2023-08-21 16:43:14 -0700

    Make me Win

---
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+export LD_PRELOAD=./winning.so
+./gm 9 8 10 24 75 9
---
+++ 0x18-dynamic_libraries/winning.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int get_number(int idx)
+{
+	if (idx == 0)
+		return 9;
+	if (idx == 1)
+		return 8;
+	if (idx == 2)
+		return 10;
+	if (idx == 3)
+		return 24;
+	if (idx == 4)
+		return 75;
+	if (idx == 5)
+		return 9; 
+	return 0;
+}
